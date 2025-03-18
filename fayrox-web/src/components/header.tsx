09079071f47be9d276f94401c288f0c2d4584fd1
rileyhorrix <riley@horrix.com>
2025-03-18 21:37:57 +0000

    final main page almost done: issue with expanding page and burger menu expansion

--- fayrox-web/src/components/header.tsx
+++ fayrox-web/src/components/header.tsx
@@ -2,10 +2,26 @@
 
 import { useEffect, useState } from "react";
 import styles from "./header.module.css"
-import { List } from "react-bootstrap-icons";
+import { List } from "react-bootstrap-icons"
 
 interface HeaderProps {
 }
+
+interface PageData {
+  name: string,
+  link: string
+}
+
+const pages: PageData[] = [
+  {
+    name: "Get Started",
+    link: "/"
+  },
+  {
+    name: "Contact",
+    link: "/"
+  }
+]
 
 export default function Header({ }: HeaderProps) {
   const [burgerExpanded, setBurgerExpanded] = useState<boolean>(false);
@@ -32,11 +48,18 @@ export default function Header({ }: HeaderProps) {
           Fayrox
         </h1>
         <List className={styles.header_menu} onClick={() => setBurgerExpanded(prev => !prev)} />
+        <div className={styles.navbar_long_wrapper}>
+          {pages.map((page) => (
+            <a key={page.name} href={page.link} className={styles.navbar_long}>{page.name}</a>
+          ))}
+        </div>
       </div>
       <div className={styles.navbar_wrapper}>
         <div className={`${styles.navbar} ${burgerExpanded ? styles.navbar_expanded : ''}`}>
-          <div className={styles.navbar_element}>Get Started</div>
-          <div className={styles.navbar_element}>Contact</div>
+          {pages.flatMap((page, index) => [
+            <div className={index !== 0 ? "border_top" : ""} style={{width: "75%", margin: "auto"}}></div>,
+            <a key={page.name} href={page.link} className={styles.navbar_element}>{page.name}</a>
+          ])}
         </div>
       </div>
     </header>
